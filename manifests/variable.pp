b56962db125b96ed81bf46f7d3c2f1f6c50fa2bd
indrajeet <indra484ku@gmail.com>
2018-12-24 02:19:41 +0530

    sdff

--- manifests/variable.pp
+++ manifests/variable.pp
@@ -1,10 +1,10 @@
 class tomcat::variable {
 
-$user => "tomcat"
-$config_path => "/etc/tomcat/tomcat.conf"
-$mode => "0777"
-$service_name => "tomcat"
-$service_status => "running"
+$user => 'tomcat'
+$config_path => '/etc/tomcat/tomcat.conf'
+$mode => '0777'
+$service_name => 'tomcat'
+$service_status => 'running'
 $packages => ['tomcat', 'tomcat-webapps']
 
 
