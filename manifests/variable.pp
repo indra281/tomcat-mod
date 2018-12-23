6cdc6598e01e78c6979fc9b89116c28961ee4841
indrajeet <indra484ku@gmail.com>
2018-12-24 02:22:20 +0530

    dsfd

--- manifests/variable.pp
+++ manifests/variable.pp
@@ -1,11 +1,11 @@
 class tomcat::variable {
 
-$user => 'tomcat'
-$config_path => '/etc/tomcat/tomcat.conf'
-$mode => '0777'
-$service_name => 'tomcat'
-$service_status => 'running'
-$packages => ['tomcat', 'tomcat-webapps']
+$user = 'tomcat'
+$config_path = '/etc/tomcat/tomcat.conf'
+$mode = '0777'
+$service_name = 'tomcat'
+$service_status = 'running'
+$packages = ['tomcat', 'tomcat-webapps']
 
 
 }
