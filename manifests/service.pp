0b345432724a4eacf03240a7054c5295d8f814e6
indrajeet <indra484ku@gmail.com>
2018-12-24 02:00:15 +0530

    dsfd

--- manifests/service.pp
+++ manifests/service.pp
@@ -9,7 +9,7 @@ enable => true,
 file { '/etc/tomcat/tomcat.conf':
 ensure => present,
 mode => '0777',
-source => 'puppet:///modules/tomcat/files/tomcat',
+source => 'puppet:///modules/tomcat/tomcat.conf',
 notify => Service['tomcat'],
 
 }
